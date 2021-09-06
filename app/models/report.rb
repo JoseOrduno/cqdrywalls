5e7234522e67d0c6bb9d457347b9fc033731d053
aqueouspeak <jose.m.orduno@gmail.com>
2021-09-06 06:23:22 +0000

    add dependency on report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,8 +1,8 @@
 class Report < ApplicationRecord
   belongs_to :user
   belongs_to :assigned_report, class_name: :User, foreign_key: :employee_id, optional: true
-  has_many :photos
-  has_many :comments
+  has_many :photos, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true
   validates :latitude, presence: true
