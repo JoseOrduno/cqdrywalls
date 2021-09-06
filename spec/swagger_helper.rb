93e6d7c6d6365ee68a1ecc62086fb494f6b8ebd6
aqueouspeak <jose.m.orduno@gmail.com>
2021-09-06 07:08:24 +0000

    f

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -94,11 +94,8 @@ RSpec.configure do |config|
           url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'localhost:3000'
-            }, 
-            production: {
               default: 'https://rocky-journey-06295.herokuapp.com/'
-            }
+            }, 
           }
         }
       ]
